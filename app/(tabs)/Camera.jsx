de4af777a91d66a36d696e01351961086abcaaad
CodingBoy16 <rs1644818@gmail.com>
2024-10-29 22:05:14 +0530

    Added The Tabbar Screen with Icon

---
+++ app/(tabs)/Camera.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Camera = () => {
+  return (
+    <View>
+      <Text>This Is An Camera</Text>
+    </View>
+  )
+}
+
+export default Camera
--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,33 +1,61 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
-
-
+import { Tabs } from "expo-router";
+import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import Entypo from "@expo/vector-icons/Entypo";
+import AntDesign from "@expo/vector-icons/AntDesign";
+import Feather from "@expo/vector-icons/Feather";
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 export default function TabLayout() {
-
   return (
     <Tabs>
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
+          title: "Home",
+          tabBarIcon: () => <Feather name="home" size={35} color="black" />,
           // tabBarIcon: ({ color, focused }) => (
           //   <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} /> ..Add Logo For Home Screen
           // ),
-        }
-        
-      }
-      
+        }}
       />
-      {/* <Tabs.Screen
-        name="explore"
+      {/* Chats Section */}
+      <Tabs.Screen
+        name="Chats"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
+          title: "Chats",
+          tabBarIcon: () => <Ionicons name="chatbubble-ellipses-outline" size={35} color="black" />,
+        }}
+      />
+       {/* Camera Section */}
+      <Tabs.Screen
+        name="Camera"
+        options={{
+          title: "",
+          tabBarIcon: () => (
+            <AntDesign name="pluscircleo" size={45} color="black" />
           ),
         }}
-      /> */}
+      />
+        {/* Carts Section */}
+      <Tabs.Screen
+        name="Carts"
+        options={{
+          title: "Carts",
+          tabBarIcon: () => (
+            <AntDesign name="shoppingcart" size={35} color="black" />
+          ),
+        }}
+      />
+      {/* User Section */}
+      <Tabs.Screen
+        name="User"
+        options={{
+          title: "User",
+          tabBarIcon: () => <AntDesign name="user" size={35} color="black" />,
+        }}
+      />
     </Tabs>
   );
 }
+
